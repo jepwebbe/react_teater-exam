db8762c83ab22090e4a9bf1c77d264a30ce4a935
jepwebbe <jepwebbe@gmail.com>
2023-03-06 10:28:27 +0100

    header, search

--- src/Components/Partials/Header/Nav.Styled.js
+++ src/Components/Partials/Header/Nav.Styled.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 export const NavStyled = styled.nav`
-
-  button {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  button.burger {
     display: none;
   }
   ul {
@@ -10,12 +12,33 @@ export const NavStyled = styled.nav`
     gap: 1rem;
     justify-content: center;
     padding: 0;
+    margin: 0;
+
     li a {
       color: black;
-    }
+     }
   }
   ul .active {
-    background-color: ${(props) => props.theme.colors.primaryBg};
+    color: ${(props) => props.theme.colors.primary};
+  }
+  form {
+    display: grid;
+    width: 40%;
+    justify-items: end;
+    margin-left: auto;
+    input {
+      grid-area: 1/ 1/-1 /-1;
+      border: none;
+      border-bottom: 1px solid ${(props) => props.theme.colors.theGrey};
+    }
+    button {
+      border: none;
+      grid-area: 1/ 1/-1 /-1;
+      background-color: transparent;
+      svg {
+        fill: ${(props) => props.theme.colors.theGrey};
+      }
+    }
   }
   @media (max-width: ${(props) => props.theme.breakpoints.m}) {
     button {
